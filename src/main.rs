b5fccbb9ec9bf9fa34fbf863bce9f0b22823513c
Toxikuu <toxikuu@users.noreply.github.com>
2025-05-26 02:22:18 -0500

    adjust logging settings

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,6 @@ use tracing_appender::{
 };
 use tracing_subscriber::{
     EnvFilter,
-    filter::Directive,
     fmt::time,
     prelude::*,
 };
@@ -67,13 +66,14 @@ fn log() {
     let (file_writer, guard) = tracing_appender::non_blocking(file_appender);
 
     let level = LevelFilter::from_str(&CONFIG.log_level).unwrap_or(LevelFilter::DEBUG);
-    let mut filter = EnvFilter::builder()
+    let filter = EnvFilter::builder()
         .with_default_directive(level.into())
         .with_env_var("LOG_LEVEL")
-        .from_env_lossy();
-
-    // Silence verbose debug logs for dircpy
-    filter = filter.add_directive(Directive::from_str("dircpy=warn").unwrap());
+        .from_env_lossy()
+        // Silence some loud crates
+        .add_directive("dircpy=warn".parse().unwrap())
+        .add_directive("reqwest=warn".parse().unwrap())
+        .add_directive("hyper_util=warn".parse().unwrap());
 
     // Trace-level logs are only written to stdout as they take up a lot of space
     tracing_subscriber::fmt()
